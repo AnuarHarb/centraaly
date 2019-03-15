e8d05df68d1db6a093766a3b1612e38e11d56548
Anuar Harb <anuarharb@hotmail.com>
2019-03-14 20:18:14 -0600

    get method for / respond a welcome message

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const express = require('express');
+const app = express(); //Ejecutamos express para acceder a sus metodos y funcionalidades
+const port = 8080;
+const chalk = require('chalk'); // Pinta colores en la consola
+
+
+//Rutas
+app.get('/', (req, res) => {
+  res.send('Estoy respondinedo algo');
+});
+
+// app.post()
+// app.put()
+// app.delete()
+
+
+
+// Escuchar peticiones en un puerto
+app.listen(port /*8080*/, () => {
+  console.log(`Escuchando en el puerto ${port}...`);
+  console.log(chalk.cyan(`http://localhost:${port}`));
+});
