6e7d76913c72242f927d3e615634ba4e26e41a80
Anuar Harb <anuarharb@hotmail.com>
2019-03-20 20:35:22 -0600

    mongo conected

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const products = require('./products');
 
+var password = process.env.CREDENTIAL;
+
 router.all('/', function(req, res) {
   res.json({
     products: "http://localhost:8080/api/products"
--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express(); //Ejecutamos express para acceder a sus metodos y funcionalidades
 const port = 8080;
 const chalk = require('chalk'); // Pinta colores en la consola
+const mongoose = require('mongoose');
+
+var dbUrl = "mongodb+srv://andrea:code@centraal-academy-rcmzy.gcp.mongodb.net/test?retryWrites=true";
+// "mongodb+srv://user:centraal123@centraalacademy-qsrt2.gcp.mongodb.net/test?retryWrites=true"
+mongoose.connect(dbUrl);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('connected')
+});
 
 // Escuchar peticiones en un puerto
 app.listen(port /*8080*/, () => {
